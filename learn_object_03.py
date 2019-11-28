35014a74cc61912e296f14060cbe51e25bd7b562
Drunk-Mozart <longhr@mail2.sysu.edu.cn>
2019-11-28 22:03:26 +0800

    try the class use another object

--- learn_object_03.py
+++ learn_object_03.py
@@ -11,13 +11,35 @@ class Gun:
 
         self.count += count
 
-    def fire():
+    def fire(self):
 
-        if self.count < 0:
+        if self.count <= 0:
             print("没有子弹，无法开枪")
+            return
 
         self.count -= 1
-        print("You are on fire")
+        print("%s You are on fire %d" % (self.model, self.count))
 
 
-ak47 = Gun("AK47")
+# ak47 = Gun("AK47")
+# ak47.add_bullet(8)
+# ak47.fire()
+
+
+class Soldier:
+
+    def __init__(self, name):
+        self.name = name
+        self.gun = None
+
+    def shoot(self):
+        if self.gun is None:
+            print("没有枪，无法射击")
+            return
+        self.gun.add_bullet(50)
+        self.gun.fire()
+
+xusanduo = Soldier("许三多")
+xusanduo.gun = Gun("AK47")
+# xusanduo.gun.add_bullet(8)
+xusanduo.shoot()
